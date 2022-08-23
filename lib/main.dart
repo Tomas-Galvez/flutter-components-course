1b955248eac4d62a05ec985ce43b6f3ffc1c2451
Tomas Galvez <galvez_tomas@hotmail.com>
2022-08-23 13:10:43 -0300

    home screen and routes added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 // import 'package:fl_components/screens/listview1_screen.dart';
+import 'package:fl_components/screens/screens.dart';
 import 'package:flutter/material.dart';
-import 'package:fl_components/screens/alert_screen.dart';
-import 'package:fl_components/screens/card_screen.dart';
-import 'package:fl_components/screens/listview1_screen.dart';
-import 'package:fl_components/screens/listview2_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -15,8 +12,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: const Listview2Screen(),
+      // home: const Listview2Screen(),
+      initialRoute: 'home',
       routes: {
+        'home': (context) => const HomeScreen(),
         'listview1': (context) => const Listview1Screen(),
         'listview2': (context) => const Listview2Screen(),
         'alert': (context) => const AlertScreen(),
