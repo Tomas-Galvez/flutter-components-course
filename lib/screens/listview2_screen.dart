0acbcfbf86372707ac81d969794a366ba6d76a7c
Tomas Galvez <galvez_tomas@hotmail.com>
2022-08-23 12:50:41 -0300

    listview2 added

---
+++ lib/screens/listview2_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Listview2Screen extends StatelessWidget {
+  final options = const [
+    'Megaman',
+    'Metal Gear',
+    'Super Smash',
+    'Final Fantasy'
+  ];
+
+  const Listview2Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Listview Tipo 2'),
+        elevation: 0,
+        backgroundColor: Colors.indigo,
+      ),
+      body: ListView.separated(
+        itemCount: options.length,
+        itemBuilder: (context, i) {
+          return ListTile(
+            title: Text(options[i]),
+            trailing: const Icon(
+              Icons.keyboard_arrow_right_rounded,
+              color: Colors.indigo,
+            ),
+            onTap: () {
+              final game = options[i];
+              print(game);
+            },
+          );
+        },
+        separatorBuilder: (_, __) => const Divider(),
+      ),
+    );
+  }
+}
